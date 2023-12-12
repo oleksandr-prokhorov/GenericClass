1b3cb09fa9e3b0561b5505dcf5f0c588f129c2cf
Guill-aume <aleksiz2017@gmail.com>
2023-12-11 20:43:50 -0500

    Add project files.

---
+++ GenericClass/MyGenericClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    public class MyGenericClass<T> where T : struct
+    {
+        private List<T> privateList;
+        public MyGenericClass()
+        {
+            privateList = new List<T>();
+        }
+        public void AddToList(T addedObject)
+        {
+            privateList.Add(addedObject);
+        }
+        public T ReturnItemFromList(int index)
+        {
+            return privateList[index];
+        }
+        public List<T> ReturnSortedList()
+        {
+            privateList.Sort();
+            privateList.Reverse();
+            return privateList;
+        }
+    }
+}
---
+++ GenericClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using GenericClass;
+
+MyGenericClass<int> genericClass = new MyGenericClass<int>();
+
+genericClass.AddToList(9);
+genericClass.AddToList(4);
+genericClass.AddToList(23);
+genericClass.AddToList(13);
+genericClass.AddToList(65);
+genericClass.AddToList(-8);
+
+Console.WriteLine(genericClass.ReturnItemFromList(0));
+
+foreach (var num in genericClass.ReturnSortedList())
+{
+    Console.WriteLine(num);
+}
